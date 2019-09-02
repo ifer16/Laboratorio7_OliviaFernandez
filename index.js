00377c9852b9da8b27982f2ac837e2edd10b9251
Olivia <olivia.f16@unitec.edu>
2019-09-02 10:07:25 -0600

    Laboratorio7

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const port = process.env.PORT || 3000
+const express = require('express');
+const App = express();
+
+const port = process.env.PORT || 3000;
+const options = {
+    root: __dirname
+}
+
+getHTML = (req, res) => {
+    res.sendFile('./index.html', options, (err) => {
+        if (err) throw err;
+        console.log('Sirviendo index.html');
+    });
+};
